9702556974128e62812d79f3320aad64cc39a5d3
LevLvov0820 <levlvov0820@gmail.com>
2023-09-11 15:00:23 +0300

    first commit

---
+++ src/components/search/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8cacc5a5bcmshf4dcec1f39aa123p10397ejsnb21a2d0be518',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "45d3e1dedcd0afb04baed36d891e1eed";
